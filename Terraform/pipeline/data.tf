8543f3b0c0f62738fa9149fe028fcdab67fceafa
neryaRez <neryarez@gmail.com>
2026-04-13 15:09:00 +0300

    pipeline_ready

--- Terraform/pipeline/data.tf
+++ Terraform/pipeline/data.tf
@@ -6,9 +6,8 @@ data "terraform_remote_state" "deployment" {
   backend = "s3"
 
   config = {
-    bucket         = "${var.project_name}-tfstate-${data.aws_caller_identity.current.account_id}"
-    key            = var.deployment_state_key
-    region         = var.aws_region
-    dynamodb_table = "${var.project_name}-tf-lock-${data.aws_caller_identity.current.account_id}"
+    bucket = "${var.project_name}-tfstate-${data.aws_caller_identity.current.account_id}"
+    key    = var.deployment_state_key
+    region = var.aws_region
   }
 }
